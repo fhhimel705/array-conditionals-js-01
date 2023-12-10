0823eeec1caa4bd2f0f7723be8d0a4cbe0cda183
FH Himel <fhhimel17@gmail.com>
2023-12-11 01:43:51 +0600

    added-08

---
+++ nested-conditions.js
@@ -0,0 +1,12 @@
+// you can give conditions inside a condition
+if (x < y) {
+  if (x + y == 65) {
+    console.log("its not 55");
+  } else if (x + y == 75) {
+    console.log("its 55");
+  } else {
+    console.log("its obviously 55");
+  }
+} else {
+  console.log("i dont know what it is");
+}
