9b74bce844c2c930fd400e1ece66297cd8efdcc4
FH Himel <fhhimel17@gmail.com>
2023-12-11 00:52:19 +0600

    added -02

---
+++ array.js
@@ -0,0 +1,17 @@
+// array means listing . sum of many elements
+// inisde of [] are called elemects and the serial is called index
+// index starts with 0 in js
+let numbers = [ 0 ,1 ,2, 3, 4, 5];
+// that variable declared above is an array
+console.log(numbers);
+
+// incase of word we have to use quotation and its sol called string
+let names = ['himu' , 'shakil', 'kalu' , 'boltu'];
+console.log(names);
+// if you want to change any element in an array
+names[2] = 'bagha';
+console.log(names);
+// if you want to find out index of an specific element
+console.log(names.indexOf('bagha'));
+// array is also an object
+
