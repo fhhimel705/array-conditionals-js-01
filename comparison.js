af8fa7a09b7effde3e450e5937f2b6225ac28c55
FH Himel <fhhimel17@gmail.com>
2023-12-11 01:08:37 +0600

    added-05

---
+++ comparison.js
@@ -0,0 +1,12 @@
+// if you put something conditional it will return boolean
+console.log(5>10);
+
+console.log(5<10);
+
+console.log(50==50);
+
+console.log(5!==10);
+
+console.log(5>=5);
+
+console.log(5<=10);
