10e946c153421fc7ef4829ad372272abc2174579
FH Himel <fhhimel17@gmail.com>
2023-12-11 01:42:08 +0600

    added-07

---
+++ multi-conditionals.js
@@ -0,0 +1,12 @@
+let phone = 50000;
+let bugdet = 25000;
+
+// you can add multiple conditions
+if(phone<bugdet || phone == bugdet){
+    console.log('i can buy that phone',phone>bugdet);
+}
+else if (phone > bugdet || bugdet > phone && bugdet > phone !== true) {
+  console.log("i can buy that phone :", phone < bugdet);
+} else {
+  console.log("i aint buying any phone");
+}
