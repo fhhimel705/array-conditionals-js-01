dfd5bcf9c6511b9449b9858397e238f909f67bef
FH Himel <fhhimel17@gmail.com>
2023-12-11 01:04:41 +0600

    added-04

---
+++ shift-unshift.js
@@ -0,0 +1,11 @@
+let numbers = [0, 1, 2, 3, 4, 5];
+console.log(numbers);
+// if you want to add something in the start of an array
+numbers.unshift(6);
+console.log(numbers);
+
+let names = ["himu", "shakil", "kalu", "boltu"];
+console.log(names);
+// if you want to remove somthing from an array in the first
+names.shift();
+console.log(names);
