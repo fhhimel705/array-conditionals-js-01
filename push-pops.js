3f639d50ebd51bd2549c010a1514aad9dbdc95f9
FH Himel <fhhimel17@gmail.com>
2023-12-11 01:01:39 +0600

    added-03

---
+++ push-pops.js
@@ -0,0 +1,11 @@
+let numbers = [0, 1, 2, 3, 4, 5];
+console.log(numbers);
+// if you want to add something in the last of an array
+numbers.push(6);
+console.log(numbers);
+
+let names = ["himu", "shakil", "kalu", "boltu"];
+console.log(names);
+// if you want to remove somthing from an array in the last
+names.pop();
+console.log(names);
