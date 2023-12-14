09df473e5458cd40498947b31cc6621835ae9e9b
FH Himel <fhhimel17@gmail.com>
2023-12-15 01:21:17 +0600

    added-final

---
+++ switch-case-xmpl.js
@@ -0,0 +1,18 @@
+// showing similar condition done by if else which we can do by switch case
+const color = 'green';
+if(color == 'black'){
+    console.log('color is black');
+}
+else if(color == 'blue'){
+    console.log('color is blue');
+}
+else if(color == 'white'){
+    console.log('color is white');
+}
+else if(color == 'green'){
+    console.log('color is green');
+}
+else if(color == 'black'){
+    console.log('color is black');
+}
+
---
+++ switch-case.js
@@ -0,0 +1,23 @@
+// switch
+const color = 'green';
+
+switch(color){
+    case 'black':
+        console.log('color is black');
+        break;
+    case 'white':
+        console.log('color is white');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    case 'green':
+        console.log('color is green');
+        break;
+    case 'black':
+        console.log('color is black');
+        break;
+    case 'black':
+        console.log('color is black');
+        break;
+}
